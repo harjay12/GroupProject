73935dbadc06ee8a5049ee333e8e7b703d585bba
harjay12 <57065385+harjay12@users.noreply.github.com>
2019-11-18 22:06:17 -0500

    Delete compOS.sh

--- compOS.sh
+++
@@ -1,6 +0,0 @@
-bcc -ansi -c -o kernel_c.o kernel.c
-as86 kernel.asm -o kernel_asm.o
-ld86 -o kernel -d kernel_c.o kernel_asm.o
-dd if=kernel of=diskc.img bs=512 conv=notrunc seek=3
-dd if=message.txt of=diskc.img bs=512 count=1 seek=30 conv=notrunc
-
